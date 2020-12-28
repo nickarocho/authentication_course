c4c152ec19619d39a7ea1596cac8adf127710f30
arocho <nick@boldlyfine.com>
2020-12-28 11:31:13 -0800

    Register a new user

--- src/store.js
+++ src/store.js
@@ -1,10 +1,29 @@
+import Axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: null
+  },
+  mutations: {
+    SET_USER_DATA (state, userData) {
+      state.user = userData
+      localStorage.setItem('user', JSON.stringify(userData))
+      Axios.defaults.headers.common['Authorization'] = `Bearer ${
+        userData.token
+      }`
+    }
+  },
+  actions: {
+    register ({ commit }, credentials) {
+      return Axios.post('//localhost:3000/register', credentials).then(
+        ({ data }) => {
+          commit('SET_USER_DATA', data)
+        }
+      )
+    }
+  }
 })
